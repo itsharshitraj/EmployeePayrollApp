9a1e34aa381f3e5aa11c421226340d8bd5b34bd8
Harshit <itsharshitraj@gmail.com>
2025-02-25 19:40:54 +0530

    Added logging configuration for different environments

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -5,13 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import org.springframework.web.bind.annotation.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 
 @RestController
 @RequestMapping(value = "/employee",produces = "application/json")
 public class EmployeePayrollController {
+    private static final Logger log = LoggerFactory.getLogger(EmployeePayrollController.class); // ✅ Manually declare logger
 
     @Autowired
     private EmployeePayrollService employeePayrollService;
@@ -39,5 +41,14 @@ public class EmployeePayrollController {
     @DeleteMapping("/delete/{id}")
     public String deleteEmployee(@PathVariable int id) {
         return employeePayrollService.deleteEmployee(id);
+    }
+
+    @GetMapping("/logTest")
+    public String testLogging() {
+        log.debug("This is a DEBUG log message");
+        log.info("This is an INFO log message");
+        log.warn("This is a WARN log message");
+        log.error("This is an ERROR log message");
+        return "Logging Test Completed!";
     }
 }
