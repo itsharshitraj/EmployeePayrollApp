df93dbe7557eccf21e9cb5bc149d6609f1a4d142
Harshit <itsharshitraj@gmail.com>
2025-02-26 11:09:59 +0530

    [Harshit]Add. Validation for Name field in Employee Payroll App

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,10 +1,13 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 
-import lombok.Data;
 
-@Data
 public class EmployeeDTO {
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
+
     private String name;
     public String getName() {
         return name;
